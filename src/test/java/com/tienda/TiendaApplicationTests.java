925576fcf299ad47578d757330b8104933101e0e
hmari <hmari@Escritorio.home.lla.com>
2026-04-14 11:01:46 -0600

    SEMANA 13 - COMPLETO 2

--- src/test/java/com/tienda/TiendaApplicationTests.java
+++ src/test/java/com/tienda/TiendaApplicationTests.java
@@ -2,13 +2,12 @@ package com.tienda;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test") // <- Activa el perfil "test" solo cuando se ejecutan los tests
 class TiendaApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
+
 }
