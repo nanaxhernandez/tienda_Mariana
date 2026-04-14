cabc5878b9f4ed9f3c05c9ecbf9775a6fcddd364
hmari <hmari@Escritorio.home.lla.com>
2026-04-14 10:04:05 -0600

    Commit Tienda

--- src/main/java/com/tienda/StorageConfig.java
+++ src/main/java/com/tienda/StorageConfig.java
@@ -1,10 +1,4 @@
-
 package com.tienda;
-
-/**
- *
- * @author hmari
- */
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Storage;
@@ -19,19 +13,20 @@ import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 public class StorageConfig {
+
     @Value("${firebase.json.path}")
     private String jsonPath;
-    
+
     @Value("${firebase.json.file}")
     private String jsonFile;
-    
+
     @Bean
     public Storage storage() throws IOException {
         ClassPathResource resource = new ClassPathResource(jsonPath + File.separator + jsonFile);
-        try (InputStream inputStream = resource.getInputStream())
-        {
-            GoogleCredentials credencials = GoogleCredentials.fromStream(inputStream);
-            return StorageOptions.newBuilder().setCredentials(credencials).build().getService();                       
-        }        
-    }    
+        try (InputStream inputStream = resource.getInputStream()) {
+            GoogleCredentials credentials = GoogleCredentials.fromStream(inputStream);
+            return StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+        }
+    }
+    
 }
