effd42e8db6718d89829c30e68751b69970ea6a7
hmari <hmari@Escritorio.home.lla.com>
2026-04-14 10:41:26 -0600

    SEMANA 6

--- src/main/java/com/tienda/service/ProductoService.java
+++ src/main/java/com/tienda/service/ProductoService.java
@@ -12,73 +12,73 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ProductoService {
-    
+
+    // El repositorio es final para asegurar la inmutabilidad
     private final ProductoRepository productoRepository;
     private final FirebaseStorageService firebaseStorageService;
-    
-    public ProductoService (ProductoRepository productoRepository, FirebaseStorageService firebaseStorageService){
+
+    public ProductoService(ProductoRepository productoRepository, FirebaseStorageService firebaseStorageService) {
         this.productoRepository = productoRepository;
         this.firebaseStorageService = firebaseStorageService;
     }
-    
-    @Transactional (readOnly = true)
-    public List<Producto> getProductos (boolean activo){
-        if (activo){
+
+    @Transactional(readOnly = true)
+    public List<Producto> getProductos(boolean activo) {
+        if (activo) { //Sólo activos...            
             return productoRepository.findByActivoTrue();
         }
         return productoRepository.findAll();
     }
-    
-    @Transactional (readOnly = true)
-    public Optional<Producto> getProducto (Integer idProducto){
+
+    @Transactional(readOnly = true)
+    public Optional<Producto> getProducto(Integer idProducto) {
         return productoRepository.findById(idProducto);
     }
-    
+
     @Transactional
-    public void save (Producto producto, MultipartFile imagenFile){
+    public void save(Producto producto, MultipartFile imagenFile) {
         producto = productoRepository.save(producto);
-        if (!imagenFile.isEmpty()){
+        if (!imagenFile.isEmpty()) { //Si no está vacío... pasaron una imagen...            
             try {
                 String rutaImagen = firebaseStorageService.uploadImage(
                         imagenFile, "producto",
                         producto.getIdProducto());
                 producto.setRutaImagen(rutaImagen);
                 productoRepository.save(producto);
-            }catch (IOException e){
-                
+            } catch (IOException e) {
+
             }
         }
     }
-    
+
     @Transactional
-    public void delete (Integer idProducto){
-       
-        if (!productoRepository.existsById(idProducto)){
-            
-            throw new IllegalArgumentException("La categoría con ID" + idProducto + " no existe");
-            
+    public void delete(Integer idProducto) {
+        // Verifica si la categoría existe antes de intentar eliminarlo
+        if (!productoRepository.existsById(idProducto)) {
+            // Lanza una excepción para indicar que el usuario no fue encontrado
+            throw new IllegalArgumentException("La categoría con ID " + idProducto + " no existe.");
         }
-        try{
+        try {
             productoRepository.deleteById(idProducto);
-        }catch (DataIntegrityViolationException e){
-            
+        } catch (DataIntegrityViolationException e) {
+            // Lanza una nueva excepción para encapsular el problema de integridad de datos
             throw new IllegalStateException("No se puede eliminar la producto. Tiene datos asociados.", e);
         }
     }
-    
+
     @Transactional(readOnly = true)
-    public List<Producto> consultaDerivada (double precioInf, double precioSup){
+    public List<Producto> consultaDerivada(double precioInf, double precioSup) {
         return productoRepository.findByPrecioBetweenOrderByPrecioAsc(precioInf, precioSup);
     }
-    
+
     @Transactional(readOnly = true)
-    public List<Producto> consultaJPQL(double precioInf, double precioSup){
+    public List<Producto> consultaJPQL(double precioInf, double precioSup) {
         return productoRepository.consultaJPQL(precioInf, precioSup);
     }
-    
+
     @Transactional(readOnly = true)
-    public List<Producto> consultaSQL(double precioInf, double precioSup){
-        return productoRepository.consultasSQL(precioInf, precioSup);
+    public List<Producto> consultaSQL(double precioInf, double precioSup) {
+        return productoRepository.consultaJPQL(precioInf, precioSup);
     }
-    
+
 }
